8664394ef149e45d21554cede70f1d1f597b494a
Collin Brittle <cnbrittle@gmail.com>
2020-12-06 18:14:55 -0500

    Refactor day2
    Adding some debugging boilerplate.
    
    Versioned the validator, so you can test against either (defaulting to
    the newer version).
    
    I stole the part that gets rid of the XOR from [Mouse vs Python](https://www.blog.pythonlibrary.org/2020/12/04/advent-of-code-2020-day-2/).

--- day2/day2.py
+++ day2/day2.py
@@ -1,31 +1,21 @@
-import operator
+DEBUG = True
 
-def valid_password(pass_line):
-    parsed_pass = parse_pass(pass_line)
-    return validate_pass(parsed_pass)
-
-def parse_pass(pass_line):
-    rule, _, password = pass_line.partition(': ')
+def parse_pass(line):
+    rule, _, password = line.partition(': ')
     occurs, _, char = rule.partition(' ')
     min_occurs, _, max_occurs = occurs.partition('-')
 
-    return {'min': int(min_occurs), 'max': int(max_occurs), 'char': char, 'pass': password}
+    return (int(min_occurs), int(max_occurs), char, password)
 
-def validate_pass_v1(options):
-    count = options['pass'].count(options['char'])
-    if options['min'] <= count and count <= options['max']:
-        return True
-    return False
+def validate(min_occurs, max_occurs, char, password, version='2'):
+    if version == 1:
+        return True if min_occurs <= password.count(char) <= max_occurs else False
 
-def validate_pass(options):
-    index1 = options['min'] - 1
-    index2 = options['max'] - 1
+    letters = [password[min_occurs-1], password[max_occurs-1]]
+    return True if letters.count(char) == 1 else False
 
-    match1 = options['pass'][index1] == options['char']
-    match2 = options['pass'][index2] == options['char']
+if __name__ == '__main__':
+    DEBUG = False
 
-    return operator.xor(match1, match2)
-
-with open('./input.txt') as inputfile:
-    valid = [line for line in inputfile if valid_password(line)]
-    print(len(valid))
+    with open('./input.txt') as inputfile:
+        print(len([ True for line in inputfile if validate(*parse_pass(line), 2) ]))
