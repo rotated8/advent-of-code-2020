ae8f19cad296b5a6f92effb622ed369cfd2af9ec
Collin Brittle <cnbrittle@gmail.com>
2020-12-06 17:42:36 -0500

    Makes day1 generic!
    Took me a second, but I figured out how to make the two functions one!

--- day1/day1.py
+++ day1/day1.py
@@ -1,26 +1,17 @@
 from itertools import combinations
+from math import prod
 
 DEBUG = True
 
-def pt1(entries):
-    DEBUG_STR = 'e1: {}, e2: {}, sum: {}, product: {}'
+def target_product(entries, items=2, target=2020):
+    DEBUG_STR = 'entries: {}, sum: {}, product: {}'
 
-    for entry1, entry2 in combinations(entries, 2):
+    for combination in combinations(entries, items):
         if DEBUG:
-            print(DEBUG_STR.format(entry1, entry2, entry1+entry2, entry1*entry2))
+            print(DEBUG_STR.format(combination, sum(combination), prod(combination)))
 
-        if entry1+entry2 == 2020:
-            return entry1*entry2
-
-def pt2(entries):
-    DEBUG_STR = 'e1: {}, e2: {}, e3: {}, sum: {}, product: {}'
-
-    for e1, e2, e3 in combinations(entries, 3):
-        if DEBUG:
-            print(DEBUG_STR.format(e1, e2, e3, e1+e2+e3, e1*e2*e3))
-
-        if e1+e2+e3 == 2020:
-            return e1*e2*e3
+        if sum(combination) == target:
+            return prod(combination)
 
 if __name__ == '__main__':
     with open('./input.txt') as inputfile:
@@ -32,5 +23,5 @@ if __name__ == '__main__':
         if DEBUG:
             print('Total entries:', len(entries))
 
-        print('Pt 1 product:', pt1(entries))
-        print('Pt 2 product:', pt2(entries))
+        print('Pt 1 product:', target_product(entries))
+        print('Pt 2 product:', target_product(entries, 3))
