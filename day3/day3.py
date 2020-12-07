8977eaed4a7c0c1af70e789962873b372532de7a
Collin Brittle <cnbrittle@gmail.com>
2020-12-06 23:42:20 -0500

    Refactor day3
    Adds debugging boilerplate, adjusts some vars.

--- day3/day3.py
+++ day3/day3.py
@@ -1,27 +1,39 @@
-RISE = 2
-RUN = 1
+DEBUG = True
+RISE = 1
+RUN = 3
 
-def descend(y_off, x_off, trees, counter):
-    # x_off is the total offset. Adjust that to be within the string given.
-    adj_x = x_off - (x_off//len(trees) * len(trees))
-    adj_y = y_off + 1
+def descend(y_off, x_off, counter, terrain):
+    # Bounds check y_off and x_off.
+    adj_y = y_off % RISE
+    adj_x = x_off % len(terrain)
 
-    #print(y_off, adj_y, x_off, adj_x)
-    #print((x_off//len(trees) + 1) * trees)
-    if adj_y == 1:
-        #print(x_off * '-' + '^')
-        if trees[adj_x] == '#':
+    if DEBUG:
+        print(y_off, adj_y, x_off, adj_x)
+
+    # Check for a tree, if necessary
+    if adj_y == 0:
+        if DEBUG:
+            print((x_off//len(terrain) + 1) * terrain)
+            print(x_off * '-' + '^')
+
+        if terrain[adj_x] == '#':
             counter += 1
 
-    if adj_y == RISE:
-        return (0, x_off + RUN, counter)
+        # Increment y_off, add RUN to x_off after checks. Allows us to check (0,0).
+        return (y_off + 1, x_off + RUN, counter)
 
-    return(adj_y, x_off, counter)
+    # Do not add RUN to x_off, we haven't finished RISE-ing yet.
+    return(y_off + 1, x_off, counter)
 
-with open('./input.txt') as inputfile:
-    x_offset = 0
-    y_offset = 0
-    counter = 0
-    for line in inputfile:
-        y_offset, x_offset, counter = descend(y_offset, x_offset, line.strip(), counter)
-    print(counter)
+if __name__ == '__main__':
+    DEBUG = False
+
+    with open('./input.txt') as inputfile:
+        x_offset = 0
+        y_offset = 0
+        counter = 0
+        for line in inputfile:
+            # I was not stripping the line originally, and the return threw off the length.
+            # y_off, x_off always go up. This accomodates lines of unequal length.
+            y_offset, x_offset, counter = descend(y_offset, x_offset, counter, line.strip())
+        print(counter)
