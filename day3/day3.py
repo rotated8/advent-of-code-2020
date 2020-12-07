622bb18ee433b62be2cc11635284d64d861fcef3
Collin Brittle <cnbrittle@gmail.com>
2020-12-07 00:15:40 -0500

    Refactor day3 into a class
    There was a bunch of global state that I wasn't terribly happy with.
    Sounds like a good reason to create a class! Added a counter for the
    number of spaces checked.

--- day3/day3.py
+++ day3/day3.py
@@ -1,39 +1,65 @@
 DEBUG = True
-RISE = 1
-RUN = 3
 
-def descend(y_off, x_off, counter, terrain):
-    # Bounds check y_off and x_off.
-    adj_y = y_off % RISE
-    adj_x = x_off % len(terrain)
+class Path:
+    def __init__(self, map_file, rise=1, run=1):
+        self.rise = rise
+        self.run = run
+        self.trees = 0
+        self.checks = 0
+        self.x_offset = 0
+        self.y_offset = 0
 
-    if DEBUG:
-        print(y_off, adj_y, x_off, adj_x)
+        with open(map_file) as terrain_map:
+            for line in terrain_map:
+                # Still important to strip the line.
+                self.descend(line.strip())
 
-    # Check for a tree, if necessary
-    if adj_y == 0:
+    def __str__(self):
+        fmt_str = 'right: {}, down: {}, trees: {}'
+        return fmt_str.format(self.run, self.rise, self.trees)
+
+    def descend(self, terrain):
+        # Adjust x_offset and y_offset for the length of the line, and the length of rise.
+        adj_x = self.x_offset % len(terrain)
+        adj_y = self.y_offset % self.rise
+
         if DEBUG:
-            print((x_off//len(terrain) + 1) * terrain)
-            print(x_off * '-' + '^')
+            print(self.y_offset, adj_y, self.x_offset, adj_x)
 
-        if terrain[adj_x] == '#':
-            counter += 1
+        # Check for a tree, if necessary
+        if adj_y == 0:
+            if DEBUG:
+                print((self.x_offset//len(terrain) + 1) * terrain)
+                print(self.x_offset * '-' + '^')
 
-        # Increment y_off, add RUN to x_off after checks. Allows us to check (0,0).
-        return (y_off + 1, x_off + RUN, counter)
+            self.checks += 1
+            if terrain[adj_x] == '#':
+                self.trees += 1
 
-    # Do not add RUN to x_off, we haven't finished RISE-ing yet.
-    return(y_off + 1, x_off, counter)
+            # Finished rising, so add run to x_offset.
+            self.x_offset += self.run
+
+        # Finished with this line of terrain; increment y_offset.
+        self.y_offset += 1
 
 if __name__ == '__main__':
     DEBUG = False
 
-    with open('./input.txt') as inputfile:
-        x_offset = 0
-        y_offset = 0
-        counter = 0
-        for line in inputfile:
-            # I was not stripping the line originally, and the return threw off the length.
-            # y_off, x_off always go up. This accomodates lines of unequal length.
-            y_offset, x_offset, counter = descend(y_offset, x_offset, counter, line.strip())
-        print(counter)
+    if not DEBUG:
+        p1_3 = Path('./input.txt', 1, 3)
+        p1_1 = Path('./input.txt', 1, 1)
+        p1_5 = Path('./input.txt', 1, 5)
+        p1_7 = Path('./input.txt', 1, 7)
+        p2_1 = Path('./input.txt', 2, 1)
+
+        print(p1_3, p1_1, p1_5, p1_7, p2_1, sep="\n")
+    else:
+        p1_3 = Path('./test.txt', 1, 3)
+        print(p1_3)
+
+        #p1_1 = Path('./test.txt', 1, 1)
+        #p1_5 = Path('./test.txt', 1, 5)
+        #p1_7 = Path('./test.txt', 1, 7)
+        #p2_1 = Path('./test.txt', 2, 1)
+        #print(p1_3, p1_1, p1_5, p1_7, p2_1, sep="\n")
+
