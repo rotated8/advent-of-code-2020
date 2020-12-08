60cd891ae22a57c9a7417cef37d003b0a3055efb
Collin Brittle <cnbrittle@gmail.com>
2020-12-08 03:37:09 -0500

    Solves day8 pt 2
    Adds a repair function, and some supporting funcs. Rewrote `load` to
    strip whitespace, because it was annoying printing it.

--- day8/day8.py
+++ day8/day8.py
@@ -2,6 +2,8 @@ DEBUG = True
 DEBUG_STR = 'acc: {}, step: {}, op: {}, val: {}'
 
 class Processor:
+    CHANGE_OP = { 'nop': 'jmp', 'jmp': 'nop' }
+
     def __init__(self, filename=None):
         self.instructions = list()
         self.accumulator = 0
@@ -17,7 +19,36 @@ class Processor:
 
     def load(self, filename):
         with open(filename) as inputfile:
-            self.instructions = inputfile.readlines()
+            for line in inputfile:
+                self.instructions.append(line.strip())
+
+    def repair(self):
+        for idx in range(len(self.instructions)):
+            # Get current instruction, operation.
+            old_inst = self.instructions[idx]
+            old_op = old_inst[:3]
+
+            # Change the operation, if allowed.
+            if old_op in Processor.CHANGE_OP:
+                # Construct and insert new instruction
+                new_inst = Processor.CHANGE_OP[old_op] + old_inst[3:]
+                self.instructions[idx] = new_inst
+
+                if DEBUG:
+                    print(f'idx: {idx}, new inst: {new_inst}')
+
+                self.run()
+
+                if self.terminated():
+                    # If the run completed successfully, return the result, like run() does.
+                    return self.accumulator
+                else:
+                    # If not, reset the instruction, and the processor.
+                    self.instructions[idx] = old_inst
+                    self.reset()
+
+        # If we get here, the program was either fine, or cannot be fixed.
+        return None
 
     def run(self):
         while self.program_counter not in self.visited:
@@ -26,7 +57,7 @@ class Processor:
             self.steps += 1
 
             # Parse instruction
-            instruction = self.instructions[self.program_counter].strip()
+            instruction = self.instructions[self.program_counter]
             operation, value = instruction[:3], int(instruction[4:])
 
             # Perform operation
@@ -48,11 +79,21 @@ class Processor:
         # Return the accumulator, to make the script output what we want.
         return self.accumulator
 
+    def terminated(self):
+        return True if self.program_counter == len(self.instructions) else False
+
+    def reset(self):
+        self.accumulator = 0
+        self.program_counter = 0
+        self.steps = 0
+        self.visited = set()
+
 if __name__ == '__main__':
     DEBUG = False
 
     if not DEBUG:
         print('acc:', Processor('input.txt').run()) # 1949
+        print('acc:', Processor('input.txt').repair()) # 1949
     else:
         print('acc:', Processor('test.txt').run()) # 5
-        print('acc:', Processor('test2.txt').run())
+        print('acc:', Processor('test.txt').repair()) # 8
