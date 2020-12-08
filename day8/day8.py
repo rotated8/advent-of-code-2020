fd198faf3a25ccf56003e33e01b34a495926f015
Collin Brittle <cnbrittle@gmail.com>
2020-12-08 02:48:37 -0500

    Minor refactors for day8
    Move run() out of init, so we can mess with instructions for pt 2.
    Added a test that runs past the end of instructions, and prevent that error.

--- day8/day8.py
+++ day8/day8.py
@@ -11,7 +11,6 @@ class Processor:
 
         if filename is not None:
             self.load(filename)
-            self.run()
 
     def __str__(self):
         return str(self.accumulator)
@@ -22,12 +21,15 @@ class Processor:
 
     def run(self):
         while self.program_counter not in self.visited:
+            # Loop control
             self.visited.add(self.program_counter)
             self.steps += 1
 
+            # Parse instruction
             instruction = self.instructions[self.program_counter].strip()
             operation, value = instruction[:3], int(instruction[4:])
 
+            # Perform operation
             if operation == 'acc':
                 self.accumulator += value
                 self.program_counter += 1
@@ -36,13 +38,21 @@ class Processor:
             else:
                 self.program_counter += 1
 
+            # Rather than get an index out of bounds, exit.
+            if self.program_counter >= len(self.instructions):
+                break
+
             if DEBUG:
                 print(DEBUG_STR.format(self.accumulator, self.steps, operation, value))
+
+        # Return the accumulator, to make the script output what we want.
+        return self.accumulator
 
 if __name__ == '__main__':
     DEBUG = False
 
     if not DEBUG:
-        print('acc:', Processor('input.txt')) # 1949
+        print('acc:', Processor('input.txt').run()) # 1949
     else:
-        print('acc:', Processor('test.txt')) # 5
+        print('acc:', Processor('test.txt').run()) # 5
+        print('acc:', Processor('test2.txt').run())
